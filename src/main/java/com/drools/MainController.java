4bae4c677a46ee5215a9da1032f98dccf9f7209a
kakapela <kacperkapela1@gmail.com>
2018-05-30 18:04:02 +0200

    Making GUI.

--- src/main/java/com/drools/MainController.java
+++ src/main/java/com/drools/MainController.java
@@ -20,57 +20,18 @@ import org.kie.api.runtime.KieSession;
 public class MainController implements  Initializable {
 
 
-    @FXML
-    private Button black;
-
-    @FXML
-    private Label label;
-
-    @FXML
-    private Button brown;
-
-    @FXML
-    private Button white;
-    @FXML
-    private Button check;
-
-
-    Gowno gowno = new Gowno();
-    Message message = new Message();
-
-    public void executeGownoRules() throws IOException,
-            DroolsParserException {
-        // Obiekt typu KieServices to singleton umożliwiający dostęp do usług Kie.
-        KieServices ks = KieServices.Factory.get();
-        BasicConfigurator.configure();
-        Logger.getLogger(MainController.class).setLevel(Level.OFF);
-        // Pobierz kontener - obiekt grupujący wszystkie bazy wiedzy danego modułu.
-        KieContainer kContainer = ks.getKieClasspathContainer();
-
-        // Obiekt sesji pozwala na interakcję z silnikiem Droolsa
-        // i jest wypełniany początkowymi obiektami.
-        KieSession session = kContainer.newKieSession("ksession-rules");
-
-        label.setText(message.getPrintMessage());
-        // Dodaj do pamięci roboczej obiekt typu gowno i message
-        session.insert(gowno);
-        session.insert(message);
-
-        // Uruchom wnioskowanie.
-        session.fireAllRules();
-    }
 
     @FXML
 public void check(MouseEvent event) throws IOException, DroolsParserException {
-        executeGownoRules();
+
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        white.setOnAction(e->gowno.setKolor("biały"));
-        brown.setOnAction(e->gowno.setKolor("brazowy"));
-        black.setOnAction(e->gowno.setKolor("czarny"));
+       // white.setOnAction(e->gowno.setKolor("biały"));
+        //brown.setOnAction(e->gowno.setKolor("brazowy"));
+        //black.setOnAction(e->gowno.setKolor("czarny"));
 
     }
 }
